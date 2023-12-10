d496e7d5982b7bf116d3294c1c63486993386577
lucas-vicenzzo <lucas.kartz.vs@gmail.com>
2023-12-10 18:15:07 -0300

    Finished Project

--- app/src/main/java/com/example/photoapp/MainActivity.java
+++ app/src/main/java/com/example/photoapp/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.photoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.view.View;
+import android.widget.Button;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button profile_Btn;
+    private TextView userName;
+    private final static String PREFERENCES = "preferencias";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SharedPreferences preferencias = getSharedPreferences(PREFERENCES, 0);
+
+        profile_Btn = findViewById(R.id.profileBtn);
+        userName = findViewById(R.id.userName);
+
+        String nome = preferencias.getString("nome", "Fulano(a)");
+        userName.setText(nome);
+
+
     }
+
+    public void goToProfile(View view) {
+        Intent i = new Intent(MainActivity.this, ProfileActivity.class);
+        startActivity(i);
+    }
+
 }
---
+++ app/src/main/java/com/example/photoapp/ProfileActivity.java
@@ -0,0 +1,48 @@
+package com.example.photoapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    private TextView nameField;
+    private TextView emailField;
+    private TextView phoneField;
+    private Button saveChangesBtn;
+    private final static String PREFERENCES = "preferencias";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+        SharedPreferences preferencias = getSharedPreferences(PREFERENCES, 0);
+        SharedPreferences.Editor editor = preferencias.edit();
+
+        nameField = findViewById(R.id.nameField);
+        emailField = findViewById(R.id.emailField);
+        phoneField = findViewById(R.id.phoneField);
+        saveChangesBtn = findViewById(R.id.saveChangesBtn);
+        saveChangesBtn.setOnClickListener((v) -> {
+            if (
+                nameField.getText().toString().equals("")
+                || emailField.getText().toString().equals("")
+                || phoneField.getText().toString().equals("")
+            ) {
+                Toast.makeText(getApplicationContext(), "Preencha os Campos Vazios", Toast.LENGTH_SHORT).show();
+            } else {
+                editor.putString("nome", nameField.getText().toString());
+                editor.putString("email", emailField.getText().toString());
+                editor.putString("fone", phoneField.getText().toString());
+                editor.commit();
+
+                Toast.makeText(getApplicationContext(), "Dados Cadastrados com Sucesso", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
